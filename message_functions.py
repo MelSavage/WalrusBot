754c75b9186e61459e82bd9ab8bb345908985fab
isentil <the_ruiner666@hotmail.com>
2017-10-04 22:35:59 -0400

    image picker working, expects .jpg for now.

--- message_functions.py
+++ message_functions.py
@@ -1,9 +1,20 @@
 import discord
 import asyncio
+from random import randint
 
 async def ping(client, message):
     await client.send_message(message.channel, 'Pong!')
 
 async def mushroom(client, message):
-    with open('images/shroom/1.jpg', 'rb') as f:
+    image_path = image_picker('shroom', 58)
+    with open(image_path, 'rb') as f:
         await client.send_file(message.channel, f)
+
+def image_picker(folder, num_pictures):
+    """
+    Generate address for image dynamically. Needs subfolder name and
+    number of pictures arguments
+    """
+    pic_num = randint(1, num_pictures)
+    image_path = "images/" + folder + "/" + str(pic_num) + ".jpg"
+    return image_path
